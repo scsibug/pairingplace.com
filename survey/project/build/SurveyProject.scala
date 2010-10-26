90be13daebae9eff4edfbe83c2a71e5dec7c6eb6
Greg Heartsfield <scsibug@imap.cc>
2010-10-25 20:10:16 -0500

    Added template project for start of survey app.

---
+++ survey/project/build/SurveyProject.scala
@@ -0,0 +1,18 @@
+import sbt._
+
+class FermataProject(info: ProjectInfo) extends DefaultWebProject(info) {
+  val liftVersion = "2.1"
+  val scalaToolsSnapshots = "Scala-Tools Maven2 Snapshots Repository" at "http://scala-tools.org/repo-snapshots/"
+  val mavenLocal = "Local Maven Repository" at "file://"+Path.userHome+"/.m2/repository"
+
+  override def libraryDependencies = Set(
+    "net.liftweb" %% "lift-webkit" % liftVersion % "compile->default",
+    "net.liftweb" %% "lift-mapper" % liftVersion % "compile->default",
+    "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default",
+    "junit" % "junit" % "4.7" % "test->default",
+    "org.scala-tools.testing" % "specs_2.8.0.RC7" % "1.6.5-SNAPSHOT" % "test->default",
+    "org.dbunit" % "dbunit" % "2.4.8" % "test->default",
+    "com.h2database" % "h2" % "1.2.138",
+    "commons-io" % "commons-io" % "1.4"
+  ) ++ super.libraryDependencies
+}
---
+++ survey/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -0,0 +1,67 @@
+package bootstrap.liftweb
+
+import net.liftweb._
+import util._
+import Helpers._
+
+import common._
+import http._
+import sitemap._
+import Loc._
+import mapper._
+
+import code.model._
+
+/**
+ * A class that's instantiated early and run.  It allows the application
+ * to modify lift's environment
+ */
+class Boot {
+  def boot {
+    if (!DB.jndiJdbcConnAvailable_?) {
+      val vendor = 
+	new StandardDBVendor(Props.get("db.driver") openOr "org.h2.Driver",
+                             Props.get("db.url") openOr 
+                             "jdbc:h2:survey.db;AUTO_SERVER=TRUE",
+                             Props.get("db.user"), Props.get("db.password"))
+
+      LiftRules.unloadHooks.append(vendor.closeAllConnections_! _)
+
+      DB.defineConnectionManager(DefaultConnectionIdentifier, vendor)
+    }
+
+    // Use Lift's Mapper ORM to populate the database
+    // you don't need to use Mapper to use Lift... use
+    // any ORM you want
+    Schemifier.schemify(true, Schemifier.infoF _, User)
+
+    // where to search snippet
+    LiftRules.addToPackages("code")
+
+    // Build TopLevel SiteMap
+    val entries = List(
+      Menu.i("Home") / "index" // the simple way to declare a menu
+    )
+
+    // set the sitemap.  Note if you don't want access control for
+    // each page, just comment this line out.
+    LiftRules.setSiteMap(SiteMap(entries:_*))
+
+    //Show the spinny image when an Ajax call starts
+    LiftRules.ajaxStart =
+      Full(() => LiftRules.jsArtifacts.show("ajax-loader").cmd)
+    
+    // Make the spinny image go away when it ends
+    LiftRules.ajaxEnd =
+      Full(() => LiftRules.jsArtifacts.hide("ajax-loader").cmd)
+
+    // Force the request to be UTF-8
+    LiftRules.early.append(_.setCharacterEncoding("UTF-8"))
+
+    // What is the function to test if a user is logged in?
+    LiftRules.loggedInTest = Full(() => User.loggedIn_?)
+
+    // Make a transaction span the whole HTTP request
+    S.addAround(DB.buildLoanWrapper)
+  }
+}
